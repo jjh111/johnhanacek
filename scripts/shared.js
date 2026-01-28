effad8e91c571023ce4465951b28bdfc097fca3e
John <johnhanacek@gmail.com>
2026-01-28 13:27:35 -0800

    Updating navbar

--- scripts/shared.js
+++ scripts/shared.js
@@ -27,7 +27,7 @@ function initNavigation() {
 
     // Mobile navigation toggle
     const navToggle = document.querySelector('.nav-toggle');
-    const navMenu = document.querySelector('nav ul');
+    const navMenu = document.querySelector('.nav-right');
 
     if (navToggle && navMenu) {
         navToggle.addEventListener('click', function() {
